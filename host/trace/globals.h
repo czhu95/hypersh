838a14bf90457d961d78d319699fc9a2547747df
Chenzhuo Zhu <czhu95@stanford.edu>
2020-02-26 17:15:55 -0800

    Add print macros.

--- host/trace/globals.h
+++ host/trace/globals.h
@@ -5,6 +5,25 @@
 
 #define LINE_SIZE_BYTES 64
 
+#define PLUGIN_NAME "SIFT_RECORDER"
+
+#define QEMU_VCPU_FOREACH(expr)                                        \
+    qemu_plugin_vcpu_for_each(id,                                      \
+            [](qemu_plugin_id_t id, unsigned int threadid) { expr })
+
+#define PLUGIN_PRINT_ERROR(...)                                        \
+    fprintf(stderr, "[" PLUGIN_NAME "] " __VA_ARGS__);                 \
+    fprintf(stderr, "\n");
+
+#define PLUGIN_PRINT_VCPU_ERROR(vcpu, ...)                             \
+    fprintf(stderr, "[" PLUGIN_NAME ": %u] ", vcpu);                   \
+    fprintf(stderr, __VA_ARGS__);                                      \
+    fprintf(stderr, "\n");
+
+#define PLUGIN_PRINT_INFO(...)                                         \
+    fprintf(stdout, "[" PLUGIN_NAME "] " __VA_ARGS__);                 \
+    fprintf(stdout, "\n");
+
 extern uint32_t smp_vcpus;
 extern const uint32_t FlowControlFF;
 extern Sift::Mode current_mode;
