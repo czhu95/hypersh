a31cbfc944bb9abc70111a31574c6e6746771e2a
Chenzhuo Zhu <czhu95@stanford.edu>
2020-05-26 12:03:53 -0700

    Add hypersh_printf.

--- guest/hypersh.h
+++ guest/hypersh.h
@@ -2,12 +2,25 @@
 #define HYPERSH_H_
 
 #include <unistd.h>
+#include <stdarg.h>
 
 #define HYPERCALL_MAGIC 712
 
 inline void hypersh_exec(const char *cmd) {
     volatile char c = *cmd;
     volatile int rc = syscall(HYPERCALL_MAGIC, cmd);
+}
+
+static char pbuf[128] = "p ";
+
+void hypersh_printf(const char *format, ... ) {
+
+    va_list(args);
+    va_start(args, format);
+    vsnprintf(pbuf + 2, 126, format, args);
+    va_end(args);
+
+    hypersh_exec(pbuf);
 }
 
 #endif
