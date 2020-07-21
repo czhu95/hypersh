a460527bd63dbabfc1c469d9a9c03d5084cf4341
Chenzhuo Zhu <czhu95@stanford.edu>
2020-07-21 11:55:04 -0700

    Add IO lock.

--- host/trace/globals.h
+++ host/trace/globals.h
@@ -4,21 +4,25 @@
 #include <shared_mutex>
 #include <vector>
 #include <unordered_map>
+#include <mutex>
 
 #include "sift_format.h"
 
 #define LINE_SIZE_BYTES 64
-
 #define PLUGIN_NAME "SIFT_RECORDER"
+
+static std::mutex io_mtx;
 
 #define PLUGIN_PRINT_ERROR(...)                                        \
 do {                                                                   \
+    std::lock_guard<std::mutex> guard(io_mtx);                         \
     fprintf(stderr, "[" PLUGIN_NAME "] " __VA_ARGS__);                 \
     fprintf(stderr, "\n");                                             \
 } while (0)
 
 #define PLUGIN_PRINT_VCPU_ERROR(vcpu, ...)                             \
 do {                                                                   \
+    std::lock_guard<std::mutex> guard(io_mtx);                         \
     fprintf(stderr, "[" PLUGIN_NAME ": %u] ", vcpu);                   \
     fprintf(stderr, __VA_ARGS__);                                      \
     fprintf(stderr, "\n");                                             \
@@ -26,6 +30,7 @@ do {                                                                   \
 
 #define PLUGIN_PRINT_VCPU_INFO(vcpu, ...)                             \
 do {                                                                   \
+    std::lock_guard<std::mutex> guard(io_mtx);                         \
     fprintf(stdout, "[" PLUGIN_NAME ": %u] ", vcpu);                   \
     fprintf(stdout, __VA_ARGS__);                                      \
     fprintf(stdout, "\n");                                             \
@@ -33,6 +38,7 @@ do {                                                                   \
 
 #define PLUGIN_PRINT_INFO(...)                                         \
 do {                                                                   \
+    std::lock_guard<std::mutex> guard(io_mtx);                         \
     fprintf(stdout, "[" PLUGIN_NAME "] " __VA_ARGS__);                 \
     fprintf(stdout, "\n");                                             \
 } while (0)
