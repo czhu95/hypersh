fa337c0114cfc2a19d5d2af628e1b1b59dc3f661
Chenzhuo Zhu <czhu95@stanford.edu>
2020-08-06 17:21:59 +0000

    Update guesh hypersh header.

--- guest/hypersh.h
+++ guest/hypersh.h
@@ -3,24 +3,45 @@
 
 #include <unistd.h>
 #include <stdarg.h>
+#include <pthread.h>
 
 #define HYPERCALL_MAGIC 712
 
-inline void hypersh_exec(const char *cmd) {
-    volatile char c = *cmd;
-    volatile int rc = syscall(HYPERCALL_MAGIC, cmd);
+static int magic = 0;
+
+// static pthread_mutex_t m;
+
+static inline void hypersh_exec(const char *cmd) {
+    // pthread_mutex_lock(&m);
+    // volatile char c = *cmd;
+    syscall(HYPERCALL_MAGIC, cmd);
+    // pthread_mutex_unlock(&m);
 }
 
-static char pbuf[128] = "p ";
-
-void hypersh_printf(const char *format, ... ) {
+static inline void hypersh_printf(const char *format, ... ) {
 
     va_list(args);
     va_start(args, format);
+    char pbuf[128] = "p ";
     vsnprintf(pbuf + 2, 126, format, args);
     va_end(args);
 
     hypersh_exec(pbuf);
+}
+
+static inline void set_magic() {
+    char cmd[50] = "";
+    snprintf(cmd, 50, "trace magic %p", &magic);
+    hypersh_exec(cmd);
+}
+
+static inline void hypercall() {
+    int src = 0;
+    __asm__ __volatile__ (
+    "cmpxchgl %1, %0;\t"
+    : "=m" (magic)
+    : "r" (src)
+    : "memory");
 }
 
 #endif
